0227a5a702cac6b5f682cc8b82637a8d39c9f607
pofox <66141963+pofox@users.noreply.github.com>
2024-12-03 00:54:26 +0200

    .

--- build/Lane.cpp
+++ build/Lane.cpp
@@ -11,14 +11,14 @@ Lane::Lane(LaneType type, sf::Vector2f size,TextureLoader& textureLoader) {
     
 
     laneType = type;
-    laneTile.setSize(size);
+    //laneTile.setSize(size);
     
     switch (type) {
     case resting:
         laneTile.setFillColor(sf::Color::Green);
         break;
     case road:
-        laneTile.setTexture(&textureLoader.landTexture);
+        landSprit.setTexture(textureLoader.landTexture);
         break;
     case water:
         laneTile.setFillColor(sf::Color::Blue);
@@ -31,8 +31,8 @@ Lane::Lane(LaneType type, sf::Vector2f size,TextureLoader& textureLoader) {
 
 void Lane::render(sf::RenderWindow& window, int index)
 {
-	//landSprit.setPosition(sf::Vector2f(0, (9 - index) * laneTile.getSize().y));
+	landSprit.setPosition(sf::Vector2f(0, (9 - index) * 80));
     laneTile.setPosition(sf::Vector2f(0, (9 - index) * laneTile.getSize().y));
-    //window.draw(landSprit);
+    window.draw(landSprit);
     window.draw(laneTile);
 }
