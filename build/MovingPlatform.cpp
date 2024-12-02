4f889fa4008fcd160235dab156b95be5c62ccbc9
pofox <66141963+pofox@users.noreply.github.com>
2024-12-02 13:32:19 +0200

    MovingPlatform class prototipe done

--- build/MovingPlatform.cpp
+++ build/MovingPlatform.cpp
@@ -1,12 +1,22 @@
 #include "MovingPlatform.h"
 
+MovingPlatform::MovingPlatform(sf::Vector2f position, sf::RectangleShape shape, sf::Vector2f size, PlatformType type) {
+    platformPostion = position;
+    platformShape = shape;
+    platformCollider.position = position;
+    platformCollider.ul = position - size / 2.f;
+    platformCollider.lr = position + size / 2.f;
+    platformSize = size;
+    platformType = type;
+}
 
+void MovingPlatform::Update(float dt, sf::Vector2f speed) {
+    platformPostion += speed * dt;
+    platformShape.setPosition(platformPostion);
+    platformCollider.updatePosition(platformPostion);
+}
 
-
-//void Particle::Update(float dt) {
-//	this->postion += this->velocity * dt;
-//}
-//
-//void Particle::addForce(Vector2d force) {
-//	this->forces += force;
-//}
+void MovingPlatform::Render(sf::RenderWindow& window)
+{
+    window.draw(platformShape);
+}
--- build/MovingPlatform.h
+++ build/MovingPlatform.h
@@ -18,18 +18,9 @@ public:
     PlatformType platformType;
 
 
-    MovingPlatform(sf::Vector2f position,sf::RectangleShape shape,sf::Vector2f size,PlatformType type) {
-        platformPostion = position;
-        platformShape = shape;
-        platformCollider.position = position;
-        platformCollider.ul = position - size / 2.f;
-        platformCollider.lr = position + size / 2.f;
-        platformSize = size;
-        platformType = type;
-    }
+    MovingPlatform(sf::Vector2f position, sf::RectangleShape shape, sf::Vector2f size, PlatformType type);
 
+    void Update(float dt, sf::Vector2f speed);
 
-    void MovingPlatform::Update(float dt) {
-
-    }
+    void Render(sf::RenderWindow& window);
 };
