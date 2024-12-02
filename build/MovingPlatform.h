4e0a88dfd1197c4d8e960040ad8bc1f118b05186
pofox <66141963+pofox@users.noreply.github.com>
2024-12-02 12:12:05 +0200

    moving platform constructor

--- build/MovingPlatform.h
+++ build/MovingPlatform.h
@@ -3,15 +3,29 @@
 #include <SFML/Graphics.hpp>
 #include <stdexcept>
 
+enum PlatformType
+{
+    Obstical,
+    Hopper
+};
+
 class MovingPlatform {
 public:
-    sf::Vector2f postion;
-    sf::RectangleShape shape;
-    AABBCollider rectangleCollider;
+    sf::Vector2f platformPostion;
+    sf::RectangleShape platformShape;
+    AABBCollider platformCollider;
+    sf::Vector2f platformSize;
+    PlatformType platformType;
 
 
-    MovingPlatform() {
-        
+    MovingPlatform(sf::Vector2f position,sf::RectangleShape shape,sf::Vector2f size,PlatformType type) {
+        platformPostion = position;
+        platformShape = shape;
+        platformCollider.position = position;
+        platformCollider.ul = position - size / 2.f;
+        platformCollider.lr = position + size / 2.f;
+        platformSize = size;
+        platformType = type;
     }
 
 
