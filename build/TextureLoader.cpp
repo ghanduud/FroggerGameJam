1494ff3f846c7eff8ed889cb3b33b0fa665348a6
pofox <66141963+pofox@users.noreply.github.com>
2024-12-03 00:19:42 +0200

    reabet

--- build/TextureLoader.cpp
+++ build/TextureLoader.cpp
@@ -5,4 +5,5 @@ TextureLoader::TextureLoader()
     if (!this->landTexture.loadFromFile("../resources/land.png")) {
         std::cout << "Error loading land texture!" << std::endl;
     }
+    landTexture.setRepeated(true);
 }
